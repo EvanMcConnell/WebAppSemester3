c132b295cfc2b3c2f448ba2b73f684304590efb4
Evan McConnell <mcconnell.evan@gmail.com>
2019-11-05 23:40:28 +0000

    .

--- CA1/webdevapp/src/App.js
+++ CA1/webdevapp/src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
+import Header from './components/layout/Header'
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 import './App.css';
 
@@ -12,21 +14,54 @@ state = {
       completed: false
     },
     {
-      id: 12,
+      id: 2,
       title: 'Dinner with wife',
       completed: false
     },
     {
-      id: 1,
+      id: 3,
       title: 'Meeting with boss',
       completed: false
     }
   ]
 }
+
+// Toggle Complete
+markComplete = (id) => {
+  this.setState( {todos: this.state.todos.map(todo => {
+    if(todo.id === id){
+      todo.completed = !todo.completed
+    }
+    return todo;
+  }) });
+}
+
+
+//Delete Todo
+delTodo = (id) => {
+  this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+}
+
+
+//Add Todo
+addTodo = (title) => {
+  const newTodo = {
+    id: 4,
+    title,
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo] });
+}
+
+
   render(){
   return (
     <div className="App">
-      <Todos />
+      <div className="container">
+      <Header />
+      <AddTodo addTodo={this.addTodo} />
+      <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
     </div>
   );
   }
--- CA1/webdevapp/src/components/Todos.js
+++ CA1/webdevapp/src/components/Todos.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
-function Todos() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+class Todos extends Component {
+markComplete = () => {}
+
+  render(){
+      return this.props.todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />
+      ));
+  }
+}
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
